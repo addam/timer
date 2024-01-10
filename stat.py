048f826b743c45a58fadd6dcde44ddc1e94525d3
Adam Dominec <adominec@gmail.com>
2024-01-10 08:44:51 +0100

    prettier stats

--- stat.py
+++ stat.py
@@ -19,27 +19,34 @@ def pretty_minute(timestamp):
     return time.strftime("%H:%M", time.localtime(timestamp))
 
 
-def main(start=0):
+def main(start=0, project=None):
+    total = 0
     for task, end in db(Log).group_by('task', end='max(end)'):
         logs = db(Log).read(task_eq=task, end_ge=start)
         if not logs:
             continue
-        print(task, pretty_duration(sum(log.end - log.start for log in logs)))
+        if project and task.project != project:
+            continue
+        task_total = sum(log.end - log.start for log in logs)
+        print(task, pretty_duration(task_total))
         group_by_day = listdict((pretty_day(log.start), log) for log in logs)
         for day, day_logs in group_by_day.items():
             print("\t", day, pretty_duration(sum(log.end - log.start for log in day_logs)))
             for log in day_logs:
                 print("\t\t", pretty_minute(log.start), "+", pretty_duration(log.end - log.start), log.description)
+        total += task_total
+    print("Total time shown:", pretty_duration(total))
 
 
 if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
         main()
-    elif argv[1] == "-h":
-        print("USAGE: stat.py [period]")
+    elif argv[1] == "-h" or argv[1] == "--help":
+        print("USAGE: stat.py [period [project]]")
         print("  period may be: 'day', 'week', 'month', 'year'")
     else:
         day = 24*3600
         period = {"day": day, "week": 7*day, "month": 31*day, "year": 366*day}[argv[1]]
-        main(time.time() - period)
+        project = argv[2] if len(argv) > 2 else None
+        main(time.time() - period, project)
